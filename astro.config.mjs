2905cb08acfab4eb0e905039d7aef11be4aaa13f
pointlessbish <pointlessbish@protonmail.com>
2024-08-21 13:57:35 +0200

    updated config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,20 @@
-import { defineConfig } from 'astro/config'
-import mdx from '@astrojs/mdx'
-import tailwind from '@astrojs/tailwind'
-import compress from 'astro-compress'
-import icon from "astro-icon"
+import { defineConfig } from 'astro/config';
+import mdx from '@astrojs/mdx';
+import tailwind from '@astrojs/tailwind';
+import compress from 'astro-compress';
+import icon from "astro-icon";
 
 // https://astro.build/config
-export default defineConfig({
+export default defineConfig( {
+  site: 'https://astronaut.github.io', // Set the site URL
+  base: '/csorter/', // Set the base path for the repo
   compressHTML: true,
-  integrations: [mdx(), icon(), tailwind({
-    applyBaseStyles: false,
-  }), compress()],
-})
+  integrations: [
+    mdx(),
+    icon(),
+    tailwind( {
+      applyBaseStyles: false,
+    } ),
+    compress(),
+  ],
+} );
